aebecb7981ccb824d3de7a617471cd8a9c5248c8
TailonR <tailrusse2020@gmail.com>
2024-08-23 11:48:13 -0800

    Upgraded to Flask App

--- simple_program.py
+++ simple_program.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
+from flask import Flask
 
-def main():
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
     return "Hello World!"
 
 if __name__=="__main__":
-    print(main())
+    app.run(debug=True)
