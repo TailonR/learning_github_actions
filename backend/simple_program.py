9604e74e54dac7f4ea5f35b2b739aaf6d8b166e2
TailonR <tailrusse2020@gmail.com>
2024-08-28 09:50:46 -0800

    Made changes to address linter issue

--- backend/simple_program.py
+++ backend/simple_program.py
@@ -6,7 +6,7 @@ app = Flask(__name__, static_folder='../frontend/public')
 
 @app.route("/hello")
 def hello_world():
-    return jsonify({'data': "Hello, I know a little more now!\n"})
+    return jsonify({"data": "Hello, I know a little more now!\n"})
 
 
 if __name__ == "__main__":
