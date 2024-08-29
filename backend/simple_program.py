04577fca8251636ff540737b3174d8194f51c1b8
TailonR <tailrusse2020@gmail.com>
2024-08-29 13:01:44 -0800

    Corrected spacing for linter

--- backend/simple_program.py
+++ backend/simple_program.py
@@ -5,6 +5,7 @@ from flask_cors import CORS
 app = Flask(__name__, static_folder="build")
 CORS(app)
 
+
 @app.route("/hello")
 def hello_world():
     return jsonify({"data": "Hello, I know a little more now!\n"})
