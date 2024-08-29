b7f6d8f3c5191b91a30460f2e1571525460a00f6
TailonR <tailrusse2020@gmail.com>
2024-08-29 15:49:32 -0800

    Updated program to serve a default path

--- backend/simple_program.py
+++ backend/simple_program.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_from_directory
 from flask_cors import CORS
+import os
 
 app = Flask(__name__, static_folder="build")
 CORS(app)
@@ -10,6 +11,13 @@ CORS(app)
 def hello_world():
     return jsonify({"data": "Hello, I know a little more now!\n"})
 
+@app.route("/", defaults={"path": ''})
+@app.route("/<path:path>")
+def serve(path):
+    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == "__main__":
     app.run()
