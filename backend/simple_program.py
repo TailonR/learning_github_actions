c6a166dc51c46534fd1031746943f0db902fe7d8
TailonR <tailrusse2020@gmail.com>
2024-09-04 10:23:31 -0800

    Updated serve to validate input paths

--- backend/simple_program.py
+++ backend/simple_program.py
@@ -15,10 +15,13 @@ def hello_world():
 @app.route("/", defaults={"path": ''})
 @app.route("/<path:path>")
 def serve(path):
-    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
-        return send_from_directory(app.static_folder, path)
-    else:
-        return send_from_directory(app.static_folder, "index.html")
+    safe_path = os.path.normpath(path)
+
+    full_path = os.path.join(app.static_folder, safe_path)
+    if os.path.commonpath([full_path, app.static_folder]) == os.path.abspath(app.static_folder):
+        if safe_path != "." and os.path.exists(full_path):
+            return send_from_directory(app.static_folder, safe_path)
+    return send_from_directory(app.static_folder, "index.html")
 
 
 if __name__ == "__main__":
