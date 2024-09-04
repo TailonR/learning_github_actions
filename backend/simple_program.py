f9ec17f8a41982d2560a27103c7b02667c316599
TailonR <tailrusse2020@gmail.com>
2024-09-04 11:29:27 -0800

    Refactored to use an allowlist

--- backend/simple_program.py
+++ backend/simple_program.py
@@ -6,6 +6,7 @@ import os
 app = Flask(__name__, static_folder="build")
 CORS(app)
 
+allowlist = [f'{app.static_folder}/', f'{app.static_folder}/index.html']
 
 @app.route("/hello")
 def hello_world():
@@ -15,16 +16,17 @@ def hello_world():
 @app.route("/", defaults={"path": ''})
 @app.route("/<path:path>")
 def serve(path):
-    if validate_path(path) and path != "":
+
+    if is_allowed(os.path.join(app.static_folder, path)):
+        if path != "":
             return send_from_directory(app.static_folder, path)
+        else:
+            return send_from_directory(app.static_folder, "index.html")
     return send_from_directory(app.static_folder, "index.html")
 
-def validate_path(path):
-    safe_path = os.path.normpath(path)
-    full_path = os.path.join(app.static_folder, safe_path)
-    common_path = os.path.commonpath([full_path, app.static_folder])
-    return common_path == os.path.abspath(app.static_folder) and \
-           os.path.exists(full_path)
+def is_allowed(path):
+    print(path)
+    return path in allowlist
 
 if __name__ == "__main__":
     app.run()
