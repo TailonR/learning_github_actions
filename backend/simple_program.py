c75691acd6ddbd954a5aa932b556c10d93b84403
TailonR <tailrusse2020@gmail.com>
2024-09-03 17:33:12 -0800

    Updated formatting based on linter

--- backend/simple_program.py
+++ backend/simple_program.py
@@ -11,13 +11,15 @@ CORS(app)
 def hello_world():
     return jsonify({"data": "Hello, I know a little more now!\n"})
 
+
 @app.route("/", defaults={"path": ''})
 @app.route("/<path:path>")
 def serve(path):
     if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
         return send_from_directory(app.static_folder, path)
     else:
-        return send_from_directory(app.static_folder, 'index.html')
+        return send_from_directory(app.static_folder, "index.html")
+
 
 if __name__ == "__main__":
     app.run()
