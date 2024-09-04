9e59d0c5e1614c89d250d0a0feb715d438901648
TailonR <tailrusse2020@gmail.com>
2024-09-04 11:03:08 -0800

    Refactored because adding a comment did not work

--- backend/simple_program.py
+++ backend/simple_program.py
@@ -15,16 +15,16 @@ def hello_world():
 @app.route("/", defaults={"path": ''})
 @app.route("/<path:path>")
 def serve(path):
-    # validate the path
-    safe_path = os.path.normpath(path)
-
-    full_path = os.path.join(app.static_folder, safe_path)
-    common_path = os.path.commonpath([full_path, app.static_folder])
-    if common_path == os.path.abspath(app.static_folder):
-        if safe_path != "." and os.path.exists(full_path):
-            return send_from_directory(app.static_folder, safe_path)
+    if validate_path(path) and path != "":
+            return send_from_directory(app.static_folder, path)
     return send_from_directory(app.static_folder, "index.html")
 
+def validate_path(path):
+    safe_path = os.path.normpath(path)
+    full_path = os.path.join(app.static_folder, safe_path)
+    common_path = os.path.commonpath([full_path, app.static_folder])
+    return common_path == os.path.abspath(app.static_folder) and \
+           os.path.exists(full_path)
 
 if __name__ == "__main__":
     app.run()
