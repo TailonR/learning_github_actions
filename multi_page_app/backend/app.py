875baadfe7a2c532abe7adeb95cf235787a7a1b2
TailonR <tailrusse2020@gmail.com>
2024-09-11 12:10:13 -0800

    Added functionality to only allow recognized routes to address CodeQL alert

--- multi_page_app/backend/app.py
+++ multi_page_app/backend/app.py
@@ -10,11 +10,19 @@ CORS(app)
 @app.route("/", defaults={"path": ""})
 @app.route("/<path:path>")
 def serve(path):
-    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
-        return send_from_directory(app.static_folder, path)
-    else:
-        return send_from_directory(app.static_folder, "index.html")
+    if is_allowed(path):
+        if path != "":
+            return send_from_directory(app.static_folder, path)
+        else:
+            return send_from_directory(app.static_folder, "index.html")
+    return send_from_directory(app.static_folder, "index.html")
 
+def is_allowed(path):
+    with open("backend/whitelist.txt", "r") as file:
+        lines = file.read().splitlines()
+        print(lines)
+        print(path in lines)
+        return path in lines
 
 if __name__ == "__main__":
     app.run()
